ba5d0988a597982dd4b1cdbb044f887e4d8a123e
Byeon JaeHyeon <nyangnyang12@naver.com>
2023-05-23 17:48:17 +0900

    05/23 서브 페이지 스크립트 작업 시작

--- js/data.js
+++ js/data.js
@@ -54,156 +54,18 @@ const Lookbook_Data = [{
   }
 ];
 
-const shoppingList = [{
-    id: "sam05",
-    name: "1호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
+const shoppingList_Top = [
   {
-    id: "sam04",
-    name: "2호",
-    price: 5000,
-    src: "./img/best_01.png"
-  },
-  {
-    id: "sam03",
-    name: "3호",
-    price: 4000,
-    src: "./img/best_02.png"
-  },
-  {
-    id: "sam02",
-    name: "4호",
-    price: 7000,
-    src: "./img/best_03.png"
-  },
-  {
-    id: "sam01",
-    name: "5호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "6호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "7호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "8호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "9호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "10호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "11호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "12호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "13호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "14호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "15호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "16호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "17호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "18호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "19호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "20호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "21호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "22호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "23호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "24호",
-    price: 9000,
-    src: "./img/best_04.png"
-  },
-  {
-    id: "sam01",
-    name: "25호",
-    price: 9000,
-    src: "./img/best_04.png"
+    id: "top_ts_m_01",
+    name: "반팔 라이프가즈 티셔츠",
+    subname:"S/S LIFEGUARDS T-SHIRT",
+    price: 75000,
+    src: ["./img/best_04.png"],
+    color:["black","white","yellow"],
+    isOut: false,
+    discountRate : 0
   }
+
 ]
 
-export {Lookbook_Data,shoppingList} 
+export {Lookbook_Data,shoppingList_Top} 
